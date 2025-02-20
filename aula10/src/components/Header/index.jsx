2fc00e2cf4d652a590d125d41cc8f1093b04b203
xexeu.l <lugabriell084@outlook.com.br>
2025-02-20 17:06:15 -0300

    Add: Adicionei a aula 2 de React

---
+++ aula10/src/components/Header/index.jsx
@@ -0,0 +1,25 @@
+import React from "react";  
+import style from "./Header.module.css"
+
+function Header(){
+    return(
+        <header className={style.header}>
+        <h1>Porco Aranha</h1>
+        <nav>
+            <ul>
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/sobre">Sobre</a>
+                </li>
+                <li>
+                    <a href="/contato">Contato</a>
+                </li>
+            </ul>
+        </nav>
+        </header>
+    )
+}
+
+export default Header;
