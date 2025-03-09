00abb10d53887f4922865c1d3fa71863cb163f14
Lucas Avelino <lugabriell084@gmail.com>
2025-03-09 16:36:02 -0300

    feat: inicializei  a atividade React com configuração Vite e criei os componentes principais

---
+++ atividadeFigma/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./pages/Home";
+import Sobre from "./pages/Sobre";
+import Contato from "./pages/Contato";
+
+function App() {
+  
+
+  return (
+<BrowserRouter>
+   <Routes>
+    <Route path = "/" element = {<Home/>}/>
+    <Route path = "/sobre" element = {<Sobre/>}/>
+    <Route path = "/contato" element = {<Contato/>}/>
+   </Routes>
+</BrowserRouter>
+  )
+}
+
+export default App
---
+++ atividadeFigma/src/pages/Home/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+
+function Home(){
+    return (
+        <>
+        <Header/>
+        <main>
+        <p>aqui é meu home</p>
+        </main>
+        <Footer/>
+        </>
+    )
+}
+
+export default Home;
